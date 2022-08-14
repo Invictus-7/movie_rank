000361613a642c8b4bab65612590529e1633b6d8
Кирилл Резник <sunlight_1091@mail.ru>
2022-08-14 15:38:29 +0200

    yamdb CI_CD 24

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='base_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
